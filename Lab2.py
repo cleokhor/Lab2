de91a1cc73a3037908aea653750fd3eec5376359
cleo khor <cleokhorjh.25@ichat.sp.edu.sg>
2026-05-04 14:46:44 +0800

    Updated calculate_bmi function

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,48 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (eg. 5,67,32)")
+def get_user_input(): 
+    numbers = input()
+    number_list = numbers.split(",")
+    float_list = [] 
+    for numbers in number_list: 
+        float_list.append(float(numbers))
+        return float_list 
+def calc_average(float_list):
+    total_sum = sum(float_list) 
+    total_numbers = len(float_list)
+    average_temp = total_sum/total_numbers
+    print("average temp", average_temp)
+    
+    return average_temp
+def calc_min_max_temperature(float_list):
+    min_temp = min(float_list)
+    max_temp = max(float_list)
+    print("min temp", min_temp)
+    print("max temp", max_temp)
+    return(min_temp,max_temp)
+
+
+def main():
+    display_main_menu()
+    float_list=get_user_input()
+    calc_average(float_list)
+    calc_min_max_temperature(float_list)
+
+
+
+if __name__ == "__main__":
+    main()
+
+  
+  
+     
+
+
+
+
+
+
+
+    
+    
--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height, weight):
     print("BMI= " + str(bmi))
     if bmi < 18.5: 
       print ("user is underweight")
+      return -1
     elif bmi <= 25.0: 
       print (" user is normal weight")
+      return 0
     else :
      print ("user is over weight")
+     return 1 
   
 
 calculate_bmi(weight=57 ,height=1.73)
